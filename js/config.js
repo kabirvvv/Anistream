de8f4985970c1f4d85411fc8740d16e86b76df69
kabirvvv <kabirv338@gmail.com>
2026-04-26 08:09:41 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // CONFIGURATION — change API_BASE to your deployed API URL
 // ============================================================
 const CONFIG = {
-  API_BASE: "https://your-api-url.com", // ← Replace with your HiAnime API URL
+  API_BASE: "https://animestream-eta.vercel.app/", // ← Replace with your HiAnime API URL
   SITE_NAME: "AniStream",
   DEFAULT_SERVER: "hd-1",
   DEFAULT_TYPE: "sub",
